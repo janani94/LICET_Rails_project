e671eb10bfdfda371371055dc1a340550c261e54
Janani <jananiveeramuthu@gmail.com>
2015-02-24 15:02:58 +0530

    blog repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "janani"=>"home#home"
+  get "about"=>"home#about"
+  get "contact"=>"home#contact"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
